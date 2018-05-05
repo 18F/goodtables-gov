703949de6c84af7b6d6a04d0b232e922cd1bed39
Catherine Devlin <catherine.devlin@gsa.gov>
2018-05-04 20:30:22 -0400

    Runs with local or online validator

--- api.py
+++ api.py
@@ -1,29 +1,72 @@
-from flask import Flask
-from flask_restful import Resource, Api, reqparse, request
-import werkzeug
+"""
+RESTful service to run a Goodtables Table Schema validator against a POSTed data file.
+
+Using no validator (applies the GoodTables default validator only)
+
+    curl -F 'file=@b01.csv'   http://localhost:5000/
+
+Using validator defined in a local file
+
+    curl -F 'schema=@table_schema.json' -F 'file=@b01.csv'   http://localhost:5000/
+
+Using validator defined at a URL
+
+    curl -F 'file=@b01.csv' -F 'schema_url=https://raw.githubusercontent.com/18F/django-data-ingest/master/examples/p02_budgets/table_schema.json' http://localhost:5000/
+
+"""
+
 import io
+import json
+
 import goodtables
+import requests
+import werkzeug
+from flask import Flask
+from flask_restful import Api, Resource, reqparse
 
 app = Flask(__name__)
 api = Api(app)
 parser = reqparse.RequestParser()
-# parser.add_argument('file')
-parser.add_argument('file', type=werkzeug.FileStorage,
-    location='files')
+parser.add_argument('file',
+                    type=werkzeug.FileStorage,
+                    location='files',
+                    help='Data file to validate.  Needs valid file extension.')
+parser.add_argument('schema',
+                    type=werkzeug.FileStorage,
+                    location='files',
+                    help='Table Schema JSON file to validate data against.')
+parser.add_argument(
+    'schema_url',
+    type=str,
+    help='URL of Table Schema JSON file to validate data against.')
 
-# , type=int, help='Rate to charge for this resource')
+
+def get_schema(post_args):
+
+    if post_args['schema']:
+        schema_file = post_args['schema'].stream
+        return json.load(schema_file)
+    elif post_args['schema_url']:
+        resp = requests.get(post_args['schema_url'])
+        return resp.json()
+
 
 class Validate(Resource):
-
     def post(self):
 
-        file = request.files['file']
-        contents = file.stream.read()
-        format = file.filename.split('.')[-1]
-        import pdb; pdb.set_trace()
-        result = goodtables.validate(io.BytesIO(contents), format=format)
+        post_args = parser.parse_args()
+
+        schema = get_schema(post_args)
+
+        contents = post_args['file'].stream.read()
+        format = post_args['file'].filename.split('.')[-1]
+        result = goodtables.validate(
+            io.BytesIO(contents),
+            format=format,
+            schema=schema)
 
         return result
+
 
 api.add_resource(Validate, '/')
 
